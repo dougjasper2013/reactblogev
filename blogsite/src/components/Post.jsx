bf0fefb403e98a0bcc7a9e4d569a21297508b3a4
Douglas Jasper <doug.jasper@trios.com>
2024-12-16 20:53:03 -0500

    Create Posts

---
+++ blogsite/src/components/Post.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const Post = () => {
+
+
+    return (
+        <div className="container my-4">
+            <h1 className="mb-4">Placeholder Title</h1>
+        </div>
+    );
+
+};
+
+export default Post;
---
+++ blogsite/src/components/PostList.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const PostList = () => {
+
+
+    return (
+        <div className="container my-4">
+            <h1 className="mb-4">Placeholder Posts</h1>
+        </div>
+    );
+
+};
+
+export default PostList;
